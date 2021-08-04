6d660f8942f867401df3b8575c9c0ab8583f4261
FirephoenixX02 <otis.schmedt@gmx.de>
2021-08-04 21:22:18 +0200

    Finished Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,9 +3,13 @@ import "../src/Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarChannel from "./SidebarChannel";
-import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import CallIcon from '@material-ui/icons/Call';
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
@@ -25,23 +29,38 @@ function Sidebar() {
           <AddIcon className="sidebar__addChannel "></AddIcon>
         </div>
         <div className="sidebar__channelsList">
-            <SidebarChannel></SidebarChannel>
-            <SidebarChannel></SidebarChannel>
-            <SidebarChannel></SidebarChannel>
-            <SidebarChannel></SidebarChannel>
+          <SidebarChannel></SidebarChannel>
+          <SidebarChannel></SidebarChannel>
+          <SidebarChannel></SidebarChannel>
+          <SidebarChannel></SidebarChannel>
         </div>
       </div>
 
       <div className="sidebar__voice">
-        <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large"/>
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
         <div className="sidebar__voiceInfo">
-            <h3>Voice Connected</h3>
-            <p>Stream</p>
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
         </div>
 
         <div className="sidebar__voiceIcons">
-            <InfoOutlinedIcon></InfoOutlinedIcon>
-            <CallIcon></CallIcon>
+          <InfoOutlinedIcon></InfoOutlinedIcon>
+          <CallIcon></CallIcon>
+        </div>
+      </div>
+      <div className="sidebar__profile">
+        <Avatar src="https://avatars.githubusercontent.com/u/81097492?s=400&u=c211c95105019c80969570ba81af0867c526a0e7&v=4"></Avatar>
+        <div className="sidebar__profileInfo">
+          <h3>Default User</h3>
+          <p>#0001</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon></MicIcon>
+          <HeadsetIcon></HeadsetIcon>
+          <SettingsIcon></SettingsIcon>
         </div>
       </div>
     </div>
